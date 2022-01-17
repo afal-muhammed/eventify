259a674c6b4e346c26e061dcded89b8defbb8fde
afal <afal@sayonetech.com>
2022-01-17 11:54:56 +0530

    change in production file

--- eventwiz/production.py
+++ eventwiz/production.py
@@ -3,19 +3,11 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # TODO:change DEBUG to false when doing final deployment
-DEBUG = True
+DEBUG = False
 
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
-
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
-)
 
 
 
